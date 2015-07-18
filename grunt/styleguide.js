25f85415d9629ed632363aaa5358a15a405c1b5e
mizuki-y <mizuki-y@syati.info>
2015-07-18 22:44:58 +0900

    Use styledocco for css document

---
+++ grunt/styleguide.js
@@ -0,0 +1,13 @@
+module.exports = {
+  options: {
+    framework: {
+      name: 'styledocco'
+    }
+  },
+  default: {
+    name: 'Style Guide',
+    files: {
+      'docs/styledocco': 'server/assets/stylesheets/**/*.less'
+    }
+  }
+}
