f6a0e0a616731943f274633316fa9faf032210df
mizuki-y <mizuki-y@syati.info>
2015-09-27 19:58:43 +0900

    Update grunt typescript settings
    for genareting a tsconfig

--- grunt/typescript.js
+++ grunt/typescript.js
@@ -4,10 +4,14 @@ module.exports = {
   default: {
       src: [path.join('<%= settings.SERVER_ROOT %>', '**/*.ts')],
       options: {
+        generateTsConfig: true,
         module: 'commonjs', //or commonjs 
         target: 'es5', //or es3 
         sourceMap: false,
-        declaration: false
+        declaration: false,
+        references: [
+          "typings/tsd.d.ts"
+        ]
       }
   }
 };
